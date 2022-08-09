ad265e5f0255e821571b000d143fa3265e42386c
Beimy <arbm159357@gmail.com>
2022-08-09 17:21:10 -0400

    about page is looking good on mobile

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,17 +7,18 @@ function About(props) {
     } = props;
     
     return(
-        <section className='flex justify-center pt-1/12 pb-80 mx-1/12 max-h-min'>
-            <div className='flex flex-col max-h-min max-w-lg columns-1 justify-center z-10'>
-                <h3 className='inline-block max-h-1/12 mb-2'>Hi There,</h3>
-                <p className='p-2 skewed-border-left bg-gray-900 min-h-min'>I'm Aaron Rones. A <a 
+        <section className='flex-col-reverse md:flex-row flex justify-center pt-1/12 pb-80 md:mx-1/12 max-h-min'>
+            <div className='flex flex-col max-w-full md:max-h-min md:max-w-lg columns-1 justify-center z-10 align-middle pt-8 md:pt-0'>
+                <h3 className='hidden md:inline-block max-h-1/12 mb-2'>Hi There,</h3>
+                <p className='md:skewed-border-left bg-gray-900 p-2 self-center max-w-[50%] md:min-h-min md:max-w-none'>I'm Aaron Rones. A <a 
                     onClick={() => {setCurrentSection('Portfolio'); setContactSelected(false);}}
                     className='border-b border-purple-800'>Full Stack Web Developer</a> I really enjoy crafting experiences for people with the help of technology. I graduated from UCF with a degree in Game Design in 2020. <br/>
                     <br />Now I'm taking what I learned about digital content creation there and applying it to Web Development.
                 </p>
             </div>
-            <div className='relative'>
-                <img className='max-h-[500px] p-8 skewed-border-right' src={require(`../../assets/favicon.png`)}/>
+            <div className=' max-w-max self-center'>
+                <h3 className='inline-block md:hidden max-h-1/12 mb-2'>Hi There,</h3>
+                <img className='max-h-[200px] md:max-h-[500px] p-8 md:skewed-border-right border-purple-800 border-2' src={require(`../../assets/favicon.png`)}/>
             </div>
         </section>
     )
---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+const About = require('./About');
+const Contact = require('./Contact');
+const Footer = require('./Footer');
+const Nav = require('./Nav');
+const Portfolio = require('./Portfolio');
+const Resume = require('./Resume');
+const Single = require('./Single');
+const Menu = require('./Menu');
+
+
+export default About; Contact; Footer; Nav; Portfolio; Resume; Single; Menu;
