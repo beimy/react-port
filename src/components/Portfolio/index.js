319f1e379ceeb4951e0083f80809ceff990f81e7
Beimy <arbm159357@gmail.com>
2022-08-12 18:33:26 -0400

    moved the single project loop into porfolio section

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,6 +4,37 @@ import Single from '../Single'
 function Portfolio(props) {
     const titleRef = useRef();
 
+    const projects = [
+        {
+            name: 'NoSQL Social Media Backend',
+            description: 'This project is a command line application that creates an express.js server that connects to a MongoDB database via mongoose.js. The server contains an api that simulates a basic social media application. Features include creating new users, creating links between users (friends), making posts (thoughts), commenting on posts(reactions).',
+            pic: 'nosqlbackend_screenshot.PNG',
+            techs: ['Javascript', 'Node.js', 'Express.js', 'MongoDB', 'Mongoose.js'],
+            link: 'https://github.com/beimy/social-network-nosql'
+        }, 
+        {
+            name: 'Just Tech News',
+            description: 'This is a simple blog style website that uses an express.js server to connect to a SQL database. Users can create posts, make friends, and edit posts. This was my first use of the MVC paradigm, using handlebars to cut down on repetitive code.',
+            pic: 'mvc-lesson-snapshot.PNG',
+            techs: ['Javascript', 'SQL', 'Sequelize',],
+            link: 'https://github.com/beimy/just-tech-news'
+        },
+        {
+            name: 'SQL Store Backend',
+            description: 'This application sets up a backend meant for an online storefront. Users can use the api endpoints to read, write, update, and delete data from a MySQL database, via an express server making use of the sequelize package.',
+            pic: 'store-backend_screenshot.PNG',
+            techs: ['Javascript', 'SQL', 'Node.js'],
+            link: 'https://github.com/beimy/store-backend'
+        },
+        {
+            name: 'Note Taker',
+            description: 'This simple website allows users to take notes. Users can save notes to a database, and also delete unneeded notes.',
+            pic: 'noteTaker_still.png',
+            techs: ['Javascript', 'SQL', 'Node.js'],
+            link: 'https://github.com/beimy/note-taker'
+        }
+    ];
+
     return (
         <section className="relative border-t-4 border-white mt-20 max-w-full p-2 md:px-10">
             <h2 ref={titleRef} className="bg-gray-900 ml-10 text-2xl md:text-7xl md:-top-12 -top-5 md:relative absolute inline-block">Portfolio</h2>
@@ -34,6 +65,17 @@ function Portfolio(props) {
                 </a>
             </div>
 
+            <div className=" pt-1/12">
+                {projects.map(project => (
+                    <Single
+                        name={project.name}
+                        description={project.description}
+                        pic={project.pic}
+                        techs={project.techs}
+                        link={project.link}
+                    />
+                ))}
+            </div>
             
 
         </section>
