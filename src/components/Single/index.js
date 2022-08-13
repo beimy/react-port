8b0f1afcfc3db32822aa4679673b7524f47214c2
Beimy <arbm159357@gmail.com>
2022-08-12 20:26:51 -0400

    small portfolio projects are responsive now

--- src/components/Single/index.js
+++ src/components/Single/index.js
@@ -4,26 +4,30 @@ function Single({name, description, pic, techs, link}) {
 
 
     return(
-        <article className="mb-16" key={name}>
+        <article className="mb-16 md:px-10" key={name}>
             <div className="flex align-middle">
-                <a className="max-w-[50%]" href={link} target={'_blank'}>
-                    <img
-                        src={require(`../../assets/${pic}`)}
-                        alt={name}
-                        className='p-2 border border-purple-800'
-                    />
-                </a>
-                <div className="ml-2 max-w-[50%]">
+                <div className="md:max-w-[50%] max-w-[80%]">
+                    <h4 className="text-lg font-bold md:hidden">{name}</h4>
+                    <a className="" href={link} target={'_blank'}>
+                        <img
+                            src={require(`../../assets/${pic}`)}
+                            alt={name}
+                            className='p-2 border border-purple-800'
+                        />
+                    </a>
+                </div>
+                <div className="hidden md:block ml-2 max-w-[50%]">
                     <h4 className="border-b border-purple-800 text-lg font-bold">{name}</h4>
                     <p>{description}</p>
-
                 </div>
             </div>
-            <div className="flex ml-1 mt-1">
-                <p>Technologies Used:</p>
-                <ul className="flex px-2">
-                    {techs.map(tech => (
-                        <li className="px-1 border-l border-1">{tech}</li>
+            <div className="flex ml-1 mt-2 max-w-full">
+                <p className="hidden md:block">Technologies Used:</p>
+                <ul className="flex flex-wrap pr-2 overflow-visible">
+                    {techs.map((tech, index, array) => (
+                        <li className={`px-1 ${index == array.length -1 ? '' : 'border-r'}`}>
+                            <p className={``}>{tech}</p>
+                        </li>
                     ))}
                 </ul>
             </div>
