b63e3440f08e7629566ffe32d46701c38f36a367
Beimy <arbm159357@gmail.com>
2022-09-25 15:35:15 -0400

    fixed contact area positioning on desktop size

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -41,35 +41,21 @@ function Contact() {
             <h2 className="bg-gray-900 text-2xl absolute -top-5 right-5 md:top-[-2.75rem] md:right-52 md:text-7xl z-10 whitespace-nowrap">Let's Work Together</h2>
             <section className="flex flex-col-reverse md:justify-between justify-center my-20 px-[5%]">
                 <form id='contact-form' className="" onSubmit={handleSubmit}>
-                    <ul className="">
-                        <li className="md:contact-li-half">    
-                            <input type="text" className="contact-input" defaultValue={'Your Name'} placeholder='Name' onBlur={handleChange} name="name" />
-                        </li>
-                        <li className="md:contact-li-half">
-                            <input type="email" className="contact-input" defaultValue={'Your Email'} placeholder='Email' onBlur={handleChange} name="email" />
-                        </li>
-                        <li className="md:mr-4">
-                            <input name="subject" className='contact-input w-full' defaultValue={'Subject'} placeholder='Subject' onBlur={handleChange} rows="1"  />
-                        </li>
-                        <li className="md:mr-4">
-                            <textarea name="message" className="contact-input w-full h-auto" defaultValue={'Message'} placeholder='Message' onBlur={handleChange} rows='5'  />
-                        </li>
-                        <li>
-                            {errorMessage && (
-                            <div>
-                                <p className='error-text'>{errorMessage}</p>
-                            </div>
-                            )}
-                        </li>
-                        <li>    
-                            <button type='submit' className="float-right bg-purple-800 w-36 h-12 border-2 border-slate-600">Submit</button>
-                        </li>
-                    </ul>
+                    <input type="text" className="contact-input md:contact-li-half" placeholder='Name' onBlur={handleChange} name="name" />
+                    <input type="email" className="contact-input md:contact-li-half md:float-right md:mx-auto" placeholder='Email' onBlur={handleChange} name="email" />
+                    <input name="subject" className='contact-input' placeholder='Subject' onBlur={handleChange} rows="1"  />
+                    <textarea name="message" className="contact-input h-auto pt-2" placeholder='Message' onBlur={handleChange} rows='5'  />
+                    
+                    <div className="px-2">
+                        <p className='error-text'>{errorMessage}</p>
+                    </div>
+                    
+                    <button type='submit' className="float-right bg-purple-800 w-36 h-12 border-2 border-slate-600">Submit</button>
                 </form>
+                <p className="px-1 pb-2">Feel free to reach out for anything from buisness inquiries to general questions. I'm always looking for new connections.</p>
                 <span className="px-1 pb-2">Download my full resume 
                     <a className=" font-bold" href={require('../../assets/Resume_AaronRones-WebDev.pdf')} download='AaronRones_Resume_WebDev.pdf'> here</a>
                 </span>
-                <p className="px-1 pb-2">Feel free to reach out for anything from buisness inquiries to general questions. I'm always looking for new connections.</p>
             </section>
 
         </div>
